08a8109fc4843f440305e3441bba893684bc804a
GuadalupeCuarenta <156109910+GuadalupeCuarenta@users.noreply.github.com>
2024-02-24 00:57:59 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,67 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Player one":return img`
+....................
+....................
+....................
+....................
+....................
+......f.............
+.....f.ff...........
+....fdffdffff.......
+....fdddddddff......
+...fdddddddddff.....
+...fddfdddddddf.....
+..fdddddddddddf.....
+..fddddddddfddf.....
+..fdddddddddddf.....
+..fddffdddddddf.....
+..fdddfdfddddff.....
+..fdddfffddddf......
+..ffddddddddff......
+...fffffffff........
+....................
+`;
+            case "image2":
+            case "platform":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . e e e e e e e e e e e e . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
